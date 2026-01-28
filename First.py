270c99bc0b87be90dcbe8c3c239a40712a6642fa
MrManoj124 <manojmanorooban@gmail.com>
2026-01-28 22:46:45 +0530

    check the newline and tab operations

--- First.py
+++ First.py
@@ -1,4 +1,24 @@
-print("Hey Manorooban")
+# print("Hey Manorooban")
 
-print("Welcome to First.py file")
+# print("Welcome to First.py file")
 
+#print("Python","is a", "widely","used","language")
+
+#output
+#Hey Manorooban
+#Welcome to First.py file
+#Python is a widely used language
+
+#rint("Python","is a", "widely","used","language",sep=".")
+#Python.is a.widely.used.language
+
+print("Escape sequence")
+print("Ben Ten Said, \n \t \"It's hero time.!\"")
+# Escape sequence
+# Ben Ten Said, "It's hero time.!"
+
+print('Dharsh said ,"This is my phone"')
+#Dharsh said ,"This is my phone"
+
+print("This is a backslash..\\")
+#This is a backslash..\
