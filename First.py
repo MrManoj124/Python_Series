ff186fe92e4f0b326ef2cba5c6bcee6329fe17d6
MrManoj124 <manojmanorooban@gmail.com>
2026-01-30 00:12:50 +0530

    take a user name

--- First.py
+++ First.py
@@ -1,44 +1,46 @@
-# print("Hey Manorooban")
+# # print("Hey Manorooban")
 
-# print("Welcome to First.py file")
+# # print("Welcome to First.py file")
 
-#print("Python","is a", "widely","used","language")
+# #print("Python","is a", "widely","used","language")
 
-#output
-#Hey Manorooban
-#Welcome to First.py file
-#Python is a widely used language
+# #output
+# #Hey Manorooban
+# #Welcome to First.py file
+# #Python is a widely used language
 
-#rint("Python","is a", "widely","used","language",sep=".")
-#Python.is a.widely.used.language
+# #rint("Python","is a", "widely","used","language",sep=".")
+# #Python.is a.widely.used.language
 
-print("Escape sequence")
-print("Ben Ten Said, \n \t \"It's hero time.!\"")
-# Escape sequence
-# Ben Ten Said, "It's hero time.!"
+# print("Escape sequence")
+# print("Ben Ten Said, \n \t \"It's hero time.!\"")
+# # Escape sequence
+# # Ben Ten Said, "It's hero time.!"
 
-print('Dharsh said ,"This is my phone"')
-#Dharsh said ,"This is my phone"
+# print('Dharsh said ,"This is my phone"')
+# #Dharsh said ,"This is my phone"
 
-print("This is a backslash..\\")
-#This is a backslash..\
+# print("This is a backslash..\\")
+# #This is a backslash..\
 
 
-#Data Types
-x = 10        # int
-y = 3.5       # float
-z = "Hello"   # string
-is_ml_fun = True  # boolean
+# #Data Types
+# x = 10        # int
+# y = 3.5       # float
+# z = "Hello"   # string
+# is_ml_fun = True  # boolean
  
 
-#mini practice
-name = "Your Name"
-age = 22
-height = 5.8
+# #mini practice
+# name = "Your Name"
+# age = 22
+# height = 5.8
 
-print("Name:", name)
-print("Age:", age)
-print("Height:", height)
+# print("Name:", name)
+# print("Age:", age)
+# print("Height:", height)
 
 
 #Take input from user and operators
+name = input("Enter Your name :")
+print("Your name is  : ", name)
