ad4d5befaff1e4a9b8226d55d884447756631b79
MrManoj124 <manojmanorooban@gmail.com>
2026-01-30 00:16:51 +0530

    key operations in python

--- First.py
+++ First.py
@@ -59,3 +59,15 @@ print(age + 5)
 #convert to float
 height = float(input("Enter your height: "))
 print(height)
+
+
+#oprations in python
+a = 10
+b = 3
+
+print(a + b)   # 13
+print(a - b)   # 7
+print(a * b)   # 30
+print(a / b)   # 3.333
+print(a % b)   # 1
+print(a ** b)  # 1000
