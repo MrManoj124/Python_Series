19212152d703f47476fe0168192743356d84fd9e
MrManoj124 <manojmanorooban@gmail.com>
2026-02-24 23:50:43 +0530

    Add Any Iterable to the list

--- List02.py
+++ List02.py
@@ -27,3 +27,12 @@ Mano = ["Trust", "Love", "Educated"]
 Rooban = ["Skilled", "Learning New Things", "Loyal","Honest"]
 Mano.extend(Mano)
 print(Mano)
+
+
+#Add Any Iterable
+###The extend() method does not have to append lists, 
+#you can add any iterable object (tuples, sets, dictionaries etc.).
+Faculty = ["Bio-science", "Technoloy","Physical-Science"]
+Department = ["IT","Enviriontmental-Science","Engineering"]
+Faculty.extend(Department)
+print(Faculty)
