b4ee89dfdfcb056fba5a64c2cdb62eea97517863
MrManoj124 <manojmanorooban@gmail.com>
2026-03-04 23:37:15 +0530

    A tuple with strings, integers and boolean values

--- Tuple.py
+++ Tuple.py
@@ -33,3 +33,7 @@ print(type(thistuple))
 tuple1 = ("apple", "banana", "cherry")
 tuple2 = (1, 5, 7, 9, 3)
 tuple3 = (True, False, False)
+
+
+#A tuple with strings, integers and boolean values:
+tuple1 = ("abc", 34, True, 40, "male")
