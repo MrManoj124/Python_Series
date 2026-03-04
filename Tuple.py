e7580d734b820cffd1a290944d4db59274e75b87
MrManoj124 <manojmanorooban@gmail.com>
2026-03-04 23:37:56 +0530

    Use type function

--- Tuple.py
+++ Tuple.py
@@ -37,3 +37,8 @@ tuple3 = (True, False, False)
 
 #A tuple with strings, integers and boolean values:
 tuple1 = ("abc", 34, True, 40, "male")
+
+
+#type()
+mytuple = ("apple", "banana", "cherry")
+print(type(mytuple))
