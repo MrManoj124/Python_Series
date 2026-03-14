20ff9fe9a3af5831d2083930ae7b0ce208c4430f
MrManoj124 <manojmanorooban@gmail.com>
2026-03-14 21:17:44 +0530

    Get user input for arithmatical operations

---
+++ Calculater.py
@@ -0,0 +1,21 @@
+#This file only for create a calculter and it's functionalities
+def add(a,b):
+    return a + b
+
+def sub(a,b):
+    return a - b 
+
+def mul(a,b):
+    return a * b 
+
+def div(a,b):
+    return a / b 
+
+def mod(a,b):
+    return a % b
+
+
+#Take a user input 
+num1 = float(input("Enter First Number : "))
+operator = input("Enter operator (+,-,*,/,%) : ")
+num2 = float(input("Enter second number : "))
