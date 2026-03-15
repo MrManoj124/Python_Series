6212d798f4f5295f2f498ef1746692eba078dda5
MrManoj124 <manojmanorooban@gmail.com>
2026-03-15 23:17:28 +0530

    Check the condition for first option

--- Calculater.py
+++ Calculater.py
@@ -40,3 +40,5 @@ while True:
         num1 = float(input("Enter first number: "))
         num2 = float(input("Enter second number: "))
 
+        if choice == "1":
+            result = add(num1, num2)
