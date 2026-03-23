d54bd0127d805de1b5acd67ac4e0bc51aa87ebaf
MrManoj124 <manojmanorooban@gmail.com>
2026-03-23 21:54:14 +0530

    create clear button

--- Calculater/calculator_gui.py
+++ Calculater/calculator_gui.py
@@ -44,3 +44,5 @@ for (text , row, col) in buttons:
         btn = tk.Button(root, text=text, width=5, height=2,
                         command=lambda t=text: click_button(t))
     btn.grid(row=row, column=col, padx=5, pady=5)
+
+# Clear button
