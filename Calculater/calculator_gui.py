88644a0ad1e43fc03d2a17bd4ecb71b18168a739
MrManoj124 <manojmanorooban@gmail.com>
2026-03-25 15:10:30 +0530

    Define mainloop function

--- Calculater/calculator_gui.py
+++ Calculater/calculator_gui.py
@@ -48,3 +48,5 @@ for (text , row, col) in buttons:
 # Clear button
 clear_btn = tk.Button(root, text="C", width=22, height=2, command=clear)
 clear_btn.grid(row=5, column=0, columnspan=4)
+
+root.mainloop()
