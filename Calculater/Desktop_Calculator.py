b2a89b90fe65c536a26a34f9d64051e179d698c3
MrManoj124 <manojmanorooban@gmail.com>
2026-03-31 23:57:43 +0530

    create evaluate_expression function

--- Calculater/Desktop_Calculator.py
+++ Calculater/Desktop_Calculator.py
@@ -6,3 +6,10 @@ import speech_recognition as sr
 # ------------------ CORE LOGIC ------------------#
 
 history = []
+
+def evaluate_expression(expr):
+    try:
+        result = eval(expr)
+        history.append(F"{expr} = {result}")
+        update_history()
+        return result
