88f4008d58cbffcc3e5b0aaa584593d8afb0639b
MrManoj124 <manojmanorooban@gmail.com>
2026-04-07 22:22:07 +0530

    create calculate function

--- Ai_System/frontend/src/App.js
+++ Ai_System/frontend/src/App.js
@@ -5,5 +5,11 @@ function App(){
   const [input, setInput] = useState("");
   const [result, setResult] = useState("");
 
-  
+  const calculate = async () => {
+    try{
+      const res = await axios.post("http://127.0.0.1:8000/calculate",{
+        expr: input,
+      });
+    }
+  }
 }
