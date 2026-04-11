76063c2222557b090ebbcddc6c53e44a0d3f17c6
MrManoj124 <manojmanorooban@gmail.com>
2026-04-11 14:56:53 +0530

    create a function as chat in App.js file

--- Ai_System/frontend/src/App.js
+++ Ai_System/frontend/src/App.js
@@ -5,17 +5,21 @@ function App(){
   const [input, setInput] = useState("");
   const [result, setResult] = useState("");
 
-  const calculate = async () => {
-    try{
-      const res = await axios.post("http://127.0.0.1:8000/calculate",{
-        expr: input,
-      });
-      setResult(res.data.result);
-    }
-    catch{
-      setResult("Error");
-    }
-  };
+  // const calculate = async () => {
+  //   try{
+  //     const res = await axios.post("http://127.0.0.1:8000/calculate",{
+  //       expr: input,
+  //     });
+  //     setResult(res.data.result);
+  //   }
+  //   catch{
+  //     setResult("Error");
+  //   }
+  // };
+  
+  const chat = async () => {
+    
+  }
 
   return (
     <div style = {{ textAlign: "center", marginTop : "50px"}}>
