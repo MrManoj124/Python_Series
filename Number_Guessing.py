7646bcedcd7adafa9b831f4df8b09c2cf80b66ae
MrManoj124 <manojmanorooban@gmail.com>
2026-04-11 20:39:13 +0530

    Add a while loop

--- Number_Guessing.py
+++ Number_Guessing.py
@@ -5,3 +5,10 @@ secret_number = random.randint(1, 100)
 print("Guess the Number between 1 and 100..!")
 
 while True:
+    try:
+        guess = int(input("Enter your guess : "))
+        if guess < secret_number:
+            print("Too low! Try again.")
+
+        elif guess > secret_number:
+            print("Too high")
