9b21d8eb4ec722bd6aa63a07c6060974aabf2a7a
MrManoj124 <manojmanorooban@gmail.com>
2026-05-02 23:29:38 +0530

    Example for OR operator  and a note

--- Operator.py
+++ Operator.py
@@ -186,3 +186,13 @@ print(6 & 3)
 # The binary representation of 3 is 0011
 
 # Then the & operator compares the bits and returns 0010, which is 2 in decimal.
+
+
+#Example for OR operator
+# he | operator compares each bit and set it to 1 if one or both is 1, otherwise it is set to 0:
+print(6 | 3)
+
+# The binary representation of 6 is 0110
+# The binary representation of 3 is 0011
+
+# Then the | operator compares the bits and returns 0111, which is 7 in decimal
