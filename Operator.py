ada8ac07a2b12b38643e3621f6c7a352eed7259e
MrManoj124 <manojmanorooban@gmail.com>
2026-05-02 23:28:33 +0530

    Example for AND operator  and a note

--- Operator.py
+++ Operator.py
@@ -170,10 +170,19 @@ print (g ^ n)
 print (g | n)
 
 # NOT operator
-print (g ~ n)
+# print (g ~ n)
 
 # Zero fill left shift
 print (g << n)
 
 # Signed right shift
 print (g >> n)
+
+
+#The & operator compares each bit and set it to 1 if both are 1, otherwise it is set to 0:
+print(6 & 3)
+
+# The binary representation of 6 is 0110
+# The binary representation of 3 is 0011
+
+# Then the & operator compares the bits and returns 0010, which is 2 in decimal.
